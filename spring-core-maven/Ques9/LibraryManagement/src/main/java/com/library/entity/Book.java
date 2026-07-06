92b1ceb81f731ae0ae09150851fecd708f7cf418
Ayush Sharma <ayusharma1706@gmail.com>
2026-07-06 11:30:51 +0530

    [Ayush Sharma] Add: spring core assignment completed

--- spring-core-maven/Ques9/LibraryManagement/src/main/java/com/library/entity/Book.java
+++ spring-core-maven/Ques9/LibraryManagement/src/main/java/com/library/entity/Book.java
@@ -4,7 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-
+//Book table
 @Entity
 public class Book {
     @Id
