9177342d2aea93d06f2a1adbf07ff96cf7fc2bf5
Ayush Sharma <ayusharma1706@gmail.com>
2026-07-13 17:26:25 +0530

    [Ayush Sharma] Employee management system completed

--- spring-data-jpa-handson/EmployeeManagementSystem/src/main/java/com/cognizant/model/Department.java
+++ spring-data-jpa-handson/EmployeeManagementSystem/src/main/java/com/cognizant/model/Department.java
@@ -1,6 +1,7 @@
 package com.cognizant.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 import lombok.Data;
 
@@ -20,5 +21,6 @@ public class Department {
 
     @OneToMany(mappedBy = "department", cascade = CascadeType.ALL)
     @JsonIgnore
+    @JsonIgnoreProperties("employees")
     private List<Employee> employees;
 }
