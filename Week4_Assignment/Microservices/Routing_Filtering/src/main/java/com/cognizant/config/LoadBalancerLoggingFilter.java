57f05473751b83184d516c09c520d00bd2098a0a
Ayush Sharma <ayusharma1706@gmail.com>
2026-07-22 12:39:19 +0530

    [Ayush Sharma]Add: Assignment Completed

--- Week4_Assignment/Microservices/Routing_Filtering/src/main/java/com/cognizant/config/LoadBalancerLoggingFilter.java
+++ Week4_Assignment/Microservices/Routing_Filtering/src/main/java/com/cognizant/config/LoadBalancerLoggingFilter.java
@@ -1,5 +1,25 @@
 package com.cognizant.config;
 
-public class LoadBalancerLoggingFilter {
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
 
+@Component
+public class LoadBalancerLoggingFilter implements GlobalFilter, Ordered {
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        System.out.println("Load Balancer routing request: "
+            + exchange.getRequest().getURI());
+        return chain.filter(exchange).then(Mono.fromRunnable(() ->
+            System.out.println("Response status: "
+                + exchange.getResponse().getStatusCode())
+        ));
+    }
+
+    @Override
+    public int getOrder() { return -1; }
 }
