87ec3eac025895a012fc524841ce365d9fe47c1b
Ayush Sharma <ayusharma1706@gmail.com>
2026-07-22 13:08:33 +0530

    [Ayush Sharma]Add: Assignment Completed

--- Week4_Assignment/Microservices/Users_Orders_Management/src/main/java/com/cognizant/controllers/UserController.java
+++ Week4_Assignment/Microservices/Users_Orders_Management/src/main/java/com/cognizant/controllers/UserController.java
@@ -1,5 +1,36 @@
 package com.cognizant.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+import com.cognizant.service.*;
+import com.cognizant.models.Users;
+@RestController
+@RequestMapping("/api/users")
 public class UserController {
 
+    @Autowired
+    private UserService userService;
+
+    @PostMapping
+    public ResponseEntity<Users> createUser(@RequestBody Users user) {
+        return ResponseEntity.ok(userService.createUser(user));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Users>> getAllUsers() {
+        return ResponseEntity.ok(userService.getAllUsers());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Users> getUserById(@PathVariable Long id) {
+        return ResponseEntity.ok(userService.getUserById(id));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+        return ResponseEntity.ok("User deleted successfully");
+    }
 }
